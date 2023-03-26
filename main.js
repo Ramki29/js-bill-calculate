50fb4749f2ae9533d332cf2e8285446756c228fd
Ramki29 <ramkie7010@gmail.com>
2023-03-26 13:42:25 +0530

    add js-bill-calculate

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let tip=document.getElementById("tip");
+
+
+tip.addEventListener("input",getTip);
+function getTip() {
+	let bill=parseInt(document.getElementById("amount").value);
+	let tips=parseInt(document.getElementById("tip").value);
+	
+	let tipsAmount=(tips*bill/100);
+	document.getElementById("tip-amount").value=tipsAmount;
+	let totalAmount=bill+tipsAmount;
+	document.getElementById("total").value=`₹${totalAmount}`;
+	document.getElementById("final-rupees").innerHTML=`₹${totalAmount}`;
+}
+
